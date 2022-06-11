4241ae8acc39c4215ef4d9f4a3e72a7a445a3263
Onur Demirkıran <onrdmrkrn1@gmail.com>
2022-06-11 16:26:38 +0300

    created microservices of devhub
    ok.

---
+++ notificition/src/main/java/com/devhub/notificition/NotificitionApplication.java
@@ -0,0 +1,13 @@
+package com.devhub.notificition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificitionApplication.class, args);
+	}
+
+}
---
+++ notificition/src/test/java/com/devhub/notificition/NotificitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhub.notificition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
